4299d55b4e86fd4a98d69e0c87e2c7d9575a8fe1
msanzr <m.sanz741@gmail.com>
2018-03-21 11:31:29 +0100

    Add files via upload

---
+++ client_gaw_mlhe_mame.sh
@@ -0,0 +1,2 @@
+cd ~/MediaProcessor/gaminganywhere/bin/
+LD_LIBRARY_PATH=../../MediaProcessors_selfcontained/_install_dir_x86/lib ./ga-client ./config/client.rel.conf rtsp://127.0.0.1:8554/desktop
---
+++ server_gaw_mlhe_mame.sh
@@ -0,0 +1,2 @@
+cd ~/MediaProcessor/gaminganywhere/bin/
+LD_LIBRARY_PATH=../../MediaProcessors_selfcontained/_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
